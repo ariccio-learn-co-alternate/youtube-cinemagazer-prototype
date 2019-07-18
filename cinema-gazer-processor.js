2eedd40cb99f2b2b697d745fd78bccc34fbfd81b
Alexander Riccio <test35965+learn@gmail.com>
2019-07-18 10:44:47 -0400

    Some progress maybe

--- cinema-gazer-processor.js
+++ cinema-gazer-processor.js
@@ -1,17 +1,16 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletNode
 // https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletProcessor
-class CinemaGazerProcessor extends window.AudioWorkletProcessor {
+class CinemaGazerProcessor extends AudioWorkletProcessor {
     
     
-    // https://developers.google.com/web/updates/2017/12/audio-worklet
-    constructor(context) {
-        super(context, 'cinema-gazer-processor');
-    }
+    // // https://developers.google.com/web/updates/2017/12/audio-worklet
+    // constructor(context) {
+    //     super(context, 'cinema-gazer-processor');
+    // }
 
 
 
     process (inputs, outputs, parameters) {
-        debugger;
         return true;
     }
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import("/cinema-gazer-processor.js")
+// import("/cinema-gazer-processor.js")
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -12,13 +12,14 @@ function setupAudioFiltering(event) {
     event.preventDefault();
     // https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
     // https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletProcessor
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
     let audioContext = new AudioContext();
-    debugger;
+
     audioContext.audioWorklet.addModule('cinema-gazer-processor.js')
     const cinemaGazerNode = new AudioWorkletNode(audioContext, 'cinema-gazer-processor')
     const videoElement = document.getElementById("video-1");
     let audioSource = audioContext.createMediaElementSource(videoElement);
     // let audioFilter = audioContext.create
     cinemaGazerNode.connect(audioContext.destination);
-
+    console.log("setupAudioFiltering complete!");
 }
